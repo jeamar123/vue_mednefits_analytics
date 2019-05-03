3dfa4c0e887f146dcb290da9b59477692034a945
jkpz10 <zayas.j10@gmail.com>
2019-05-03 14:37:45 +0800

    .

---
+++ src/components/cards/timePeriod.vue
@@ -0,0 +1,17 @@
+<script>
+	var timePeriod = {
+		data() {
+			return {
+				user_data : {},
+			}
+		},
+		created() {
+
+		},
+		methods: {
+			
+	  }
+	}
+
+	export default timePeriod
+</script>
---
+++ src/views/cards/TimePeriod.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="timePeriod-container">
+		TimePeriod
+	</div>
+</template>
+
+<script>
+	import timePeriod from '@/components/timePeriod'
+	export default timePeriod
+</script>
