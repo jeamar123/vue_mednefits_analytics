bf9696bcf771b87de64a0f4771a3543279a32674
jkpz10 <zayas.j10@gmail.com>
2019-05-07 18:30:24 +0800

    update chart labels

--- src/views/cards/summary/GrowthTimePeriod.vue
+++ src/views/cards/summary/GrowthTimePeriod.vue
@@ -7,13 +7,65 @@
 
 <script>
 import Chart from "chart.js";
-import planetChartData from "../../../chart-data/chart-data.js";
 
 export default {
   data() {
     return {
-      planetChartData: planetChartData, // chart data
-      user_data: {}
+      user_data: {},
+      chartDatas: {
+        type: "bar",
+        data: {
+          labels: ["W1", "W2", "W3", "W4", "W5", "W6"],
+          datasets: [
+            {
+              // one line graph
+              label: "Time Period",
+              data: [0, 0, 1, 2, 67, 62, 27, 14],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)"
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)"
+              ],
+              borderWidth: 1
+            }
+            // { // another line graph
+            //   label: 'Planet Mass (x1,000 km)',
+            //   data: [4.8, 12.1, 12.7, 6.7, 139.8, 116.4, 50.7, 49.2],
+            //   backgroundColor: [
+            //     'rgba(71, 183,132,.5)', // Green
+            //   ],
+            //   borderColor: [
+            //     '#47b784',
+            //   ],
+            //   borderWidth: 3
+            // }
+          ]
+        },
+        options: {
+          responsive: true,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                  padding: 25
+                }
+              }
+            ]
+          }
+        }
+      } // end chart data
     };
   },
   created() {},
@@ -28,12 +80,11 @@ export default {
     }
   },
   mounted() {
-    this.createChart("crowthTimePeriod-chart", this.planetChartData);
+    this.createChart("crowthTimePeriod-chart", this.chartDatas);
   }
 };
 </script>
 
 <style>
-
 </style>
 
--- src/views/cards/summary/Life-pro-plan.vue
+++ src/views/cards/summary/Life-pro-plan.vue
@@ -11,13 +11,70 @@
 
 <script>
 import Chart from "chart.js";
-import planetChartData from "../../../chart-data/chart-data.js";
 
 export default {
   data() {
     return {
-      planetChartData: planetChartData, // chart data
-      user_data: {}
+      user_data: {},
+      chartDatas: {
+        type: "bar",
+        data: {
+          labels: ['1st Month','2nd Month', '3rd Month','4th Month', '5th Month', '6th Month'],
+          datasets: [
+            {
+              // one line graph
+              label: "Life Plan",
+              data: [0, 0, 1, 2, 67, 62, 27, 14],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)"
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)"
+              ],
+              borderWidth: 1
+            },
+            { // another line graph
+              label: 'Pro Plan',
+              data: [4.8, 12.1, 12.7, 6.7, 139.8, 116.4, 50.7, 49.2],
+              backgroundColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)"
+              ],
+              borderColor: [
+                 "rgba(255, 99, 132, 1)",
+              ],
+              borderWidth: 3
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                  padding: 25
+                }
+              }
+            ]
+          }
+        }
+      } // end chart data
     };
   },
   created() {},
@@ -32,12 +89,12 @@ export default {
     }
   },
   mounted() {
-    this.createChart("life-pro-plan-chart", this.planetChartData);
+    this.createChart("life-pro-plan-chart", this.chartDatas);
   }
 };
 </script>
 
 <style lang="scss" scoped>
-  @import './src/assets/css/home.scss'
+@import "./src/assets/css/home.scss";
 </style>
 
