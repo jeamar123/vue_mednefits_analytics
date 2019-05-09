727608722a5a5c231717a59e37e7cf542f3c279f
noelou jan <noelounagac@gmail.com>
2019-05-09 15:01:40 +0800

    add contents and design for login

--- src/components/login.vue
+++ src/components/login.vue
@@ -22,6 +22,5 @@
 </script>
 
 <style lang="scss" scoped>
-// Style here 
-
+	@import './src/assets/css/login.scss';
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,31 @@
 <template>
-  
+  <div class="container">
+  	<div class="analytics-login-container data-mednefits-box">
+  		<div class="login-header">
+  			<img :src="'../assets/img/mednefits_logo_v3_(blue)_LARGE.png'">
+  			<h2>data analytics</h2>
+  		</div>
+  		<form>
+  			<div class="analytics-input-wrapper">
+  				<input type="text" placeholder="Email address">
+  			</div>
+  			<div class="analytics-input-wrapper">
+  				<input type="text" placeholder="Enter password">
+  			</div>
+  			<div class="checkbox-wrapper">
+  				<label>
+  					<input type="checkbox"> Stay signed in
+  				</label>
+  			</div>
+  			<div>
+  				<button>Log in</button>
+  			</div>
+  			<div class="forgot-pass-wrapper">
+  				<a>Forgot password?</a>
+  			</div>
+  		</form>
+  	</div>
+  </div>
 </template>
 
 <script>
