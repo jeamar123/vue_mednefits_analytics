84a2f62444cf82e92916e5af3c473d6a3a2f6c5d
jkpz10 <zayas.j10@gmail.com>
2019-05-10 16:54:17 +0800

    add changeState to login. when click forgot and signup

--- src/components/login.vue
+++ src/components/login.vue
@@ -3,22 +3,34 @@
 //imports here
 
 // vue js here
-	var login = {
-		components: {
-		},
-		data() {
-			return {
-				user_data : {},
-			}
-		},
-		created() {
+var login = {
+  components: {},
+  data() {
+    return {
+      user_data: {},
+      loginState: "login"
+    };
+  },
+  created() {},
+  methods: {
+    changeState(data) {
+      if (data === "login") {
+        this.loginState = data;
+        //methods here
+      } else if (data === "forgot") {
+        this.loginState = data;
+        //methods here
+      } else if (data === "signup") {
+        this.loginState = data;
+        //methods here
+      }
+    }
+  },
+};
 
-		}
-	}
-
-	export default login
+export default login;
 </script>
 
 <style lang="scss" scoped>
-	@import './src/assets/css/login.scss';
+@import "./src/assets/css/login.scss";
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,69 +1,89 @@
 <template>
-	<div class="login-page">
-	  <div class="container">
-	  	<div class="analytics-login-container data-mednefits-box">
-	  		<div class="login-header">
-	  			<img :src="'../assets/img/mednefits_logo_v3_(blue)_LARGE.png'">
-	  			<h2>data analytics</h2>
-	  		</div>
-	  		<!-- for login forms -->
-	  		<form v-if="true">
-	  			<div class="analytics-input-wrapper">
-	  				<input type="text" placeholder="Email address">
-	  			</div>
-	  			<div class="analytics-input-wrapper">
-	  				<input type="text" placeholder="Enter password">
-	  			</div>
-	  			<div class="checkbox-wrapper">
-	  				<label>
-	  					<input type="checkbox"> Stay signed in
-	  				</label>
-	  			</div>
-	  			<div>
-	  				<button>Log in</button>
-	  			</div>
-	  			<div class="reset-login-wrapper">
-	  				<a>Forgot password?</a>
-	  			</div>
-	  		</form>
-	  		<!-- for forgot password form -->
-	  		<form v-if="false" class="reset-container">
-	  			<div class="analytics-input-wrapper">
-	  				<input type="text" placeholder="Communication Email address">
-	  			</div>
-	  			<div>
-	  				<button>Reset Password</button>
-	  			</div>
-	  			<div class="reset-login-wrapper">
-	  				<a>Login</a>
-	  			</div>
-	  		</form>
-	  		<!-- for sign up form -->
-	  		<form v-if="false" class="signup-container">
-	  			<div class="first-last-container">
-		  			<div class="analytics-input-wrapper">
-		  				<input type="text" placeholder="First name">
-		  			</div>
-		  			<div class="analytics-input-wrapper">
-		  				<input type="text" placeholder="Last name">
-		  			</div>
-	  			</div>
-	  			<div class="analytics-input-wrapper">
-	  				<input type="text" placeholder="Email">
-	  			</div>
-	  			<div class="analytics-input-wrapper">
-	  				<input type="text" placeholder="Password">
-	  			</div>
-	  			<div class="analytics-input-wrapper">
-	  				<input type="text" placeholder="Confrim password">
-	  			</div>
-	  			<div>
-	  				<button>Sign Up</button>
-	  			</div>
-	  		</form>
-	  	</div>
-  	</div>
-  </div>	
+  <div class="login-page">
+    <div class="container">
+      <div class="analytics-login-container data-mednefits-box">
+        <div class="login-header">
+          <img :src="'../assets/img/mednefits_logo_v3_(blue)_LARGE.png'">
+
+          <h2>data analytics</h2>
+        </div>
+
+        <!-- for login forms -->
+
+        <form v-if="loginState === 'login'">
+          <div class="analytics-input-wrapper">
+            <input type="text" placeholder="Email address">
+          </div>
+
+          <div class="analytics-input-wrapper">
+            <input type="text" placeholder="Enter password">
+          </div>
+
+          <div class="checkbox-wrapper">
+            <label>
+              <input type="checkbox">
+
+              Stay signed in
+            </label>
+          </div>
+
+          <div>
+            <button>Log in</button>
+          </div>
+
+          <div class="reset-login-wrapper">
+            <a @click="changeState('forgot')">Forgot password?</a>
+          </div>
+        </form>
+
+        <!-- for forgot password form -->
+
+        <form v-if="loginState === 'forgot'" class="reset-container">
+          <div class="analytics-input-wrapper">
+            <input type="text" placeholder="Communication Email address">
+          </div>
+
+          <div>
+            <button>Reset Password</button>
+          </div>
+
+          <div class="reset-login-wrapper">
+            <a @click="changeState('login')">Login</a>
+          </div>
+        </form>
+
+        <!-- for sign up form -->
+
+        <form v-if="loginState === 'signup'" class="signup-container">
+          <div class="first-last-container">
+            <div class="analytics-input-wrapper">
+              <input type="text" placeholder="First name">
+            </div>
+
+            <div class="analytics-input-wrapper">
+              <input type="text" placeholder="Last name">
+            </div>
+          </div>
+
+          <div class="analytics-input-wrapper">
+            <input type="text" placeholder="Email">
+          </div>
+
+          <div class="analytics-input-wrapper">
+            <input type="text" placeholder="Password">
+          </div>
+
+          <div class="analytics-input-wrapper">
+            <input type="text" placeholder="Confrim password">
+          </div>
+
+          <div>
+            <button>Sign Up</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
